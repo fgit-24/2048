79bbe89796cdaf2ea890c769f2984668e4e4bd78
Florian Hass <f91hama@gmail.com>
2024-07-28 09:10:47 +0000

    Add display function to show the current board

--- run.py
+++ run.py
@@ -11,3 +11,56 @@ Game State Management: Functions to check for win and loss conditions, add new v
 
 Game Loop: A loop that continues until the game is won or lost, prompting the user for moves and updating the board accordingly.
 '''
+
+
+# Initialize Board Size and Create Blank Board
+boardSize = 4
+
+board = []
+for i in range(boardSize):
+    row = []
+    for j in range(boardSize):
+        row.append(0)
+    board.append(row)
+
+
+# Display Function
+def display():
+    largest = board[0][0]
+    for row in board:
+        for element in row:
+            if element > largest:
+                largest = element
+
+    numSpaces = len(str(largest))
+
+    for row in board:
+        currRow = '|'
+        for element in row:
+            if element == 0:
+                currRow += " " * numSpaces + '|'
+            else:
+                currRow += (' ' * (numSpaces - len(str(element)))) + str(element) + '|'
+        print(currRow)
+    print()
+
+
+# Display Function
+def display():
+    largest = board[0][0]
+    for row in board:
+        for element in row:
+            if element > largest:
+                largest = element
+
+    numSpaces = len(str(largest))
+
+    for row in board:
+        currRow = '|'
+        for element in row:
+            if element == 0:
+                currRow += " " * numSpaces + '|'
+            else:
+                currRow += (' ' * (numSpaces - len(str(element)))) + str(element) + '|'
+        print(currRow)
+    print()
